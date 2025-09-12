7d78fc85e007b5ff5c2e6aae17549d704b194f39
SINJE2 <SINJE2@OOCL.COM>
2025-09-12 15:02:52 +0800

    init commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,18 @@
+import {TodoContext} from "../contexts/TodoContext"
+import {useContext} from "react";
+
 const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
-}
+    const value = useContext(TodoContext);
+    const {state, dispatch} = value;
+
+    return(
+        <div>
+            <div>This is the TodoList Component</div>
+            {state.map(todo => {
+                <div>{todo.text}</div>
+            })}
+        </div>
+    )
+};
 
 export default TodoList
